d4c53c3d4335843cbf5cc3586b5281cd3e2f743e
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-10-26 13:31:43 +0400

    fix: remove layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
-import AccountSwitcher from 'Components/layout/header/account-switcher';
 import AuthProvider from 'Contexts/authProvider';
 
 const App = () => (
     <AuthProvider>
         <ErrorBoundary>
-            <Layout>
-                <AccountSwitcher />
-            </Layout>
+            <Layout>App</Layout>
         </ErrorBoundary>
     </AuthProvider>
 );
