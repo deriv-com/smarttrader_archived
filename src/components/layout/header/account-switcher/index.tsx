64aa28fd89783242cca5cc4db477bc63801b4781
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-10-26 15:05:03 +0400

    fix: account switcher

--- src/components/layout/header/account-switcher/index.tsx
+++ src/components/layout/header/account-switcher/index.tsx
@@ -1,5 +1,5 @@
 const AccountSwitcher = () => (
-    <div className='px-base flex h-full cursor-pointer flex-row items-center gap-4 text-base font-bold text-success hover:bg-disabled-100'>
+    <div className='flex h-full cursor-pointer flex-row items-center gap-4 px-base text-base font-bold text-success hover:bg-disabled-100'>
         <img src='/images/pages/header/ic-currency-virtual.svg' />
         10,017.82
         <img src='/images/pages/header/ic-chevron-down.svg' />
