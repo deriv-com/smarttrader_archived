dfa933750572d2a9d69faa3c0bfb5b88602d616b
Nada <nada.ajinas@deriv.com>
2023-11-02 10:08:10 +0400

    fix: removed commented line

--- src/components/layout/header/logged-out-state/logged-out-state.tsx
+++ src/components/layout/header/logged-out-state/logged-out-state.tsx
@@ -5,8 +5,6 @@ const LoggedOutState = () => {
     const { t } = useTranslation();
     return (
         <div className='mx-base flex'>
-            {/* <Button className=' text-fontSize-xxl mr-1.5 border-none bg-white px-base text-primary hover:bg-red-50'>
-             */}
             <Button variant='contained' color='tertiary' className='mr-1.5'>
                 <a>{t('Log in')}</a>
             </Button>
