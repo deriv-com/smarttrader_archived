c8f580921839eb654fcd1c07f4311ad4a93800bd
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-11-03 10:35:32 +0800

    fix: eslint error

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment, PropsWithChildren } from 'react';
 import Header from './header';
-import Footer from './Footer/Footer';
+import Footer from './footer';
 
 const Layout = ({ children }: PropsWithChildren) => (
     <Fragment>
