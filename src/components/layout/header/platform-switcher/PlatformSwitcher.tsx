bc313448e57c8656d50f7892b4f61b2319e67b90
Nada <nada.ajinas@deriv.com>
2023-11-03 14:21:15 +0400

    fix: added alt attribute for platformswitcher icon

--- src/components/layout/header/platform-switcher/PlatformSwitcher.tsx
+++ src/components/layout/header/platform-switcher/PlatformSwitcher.tsx
@@ -4,9 +4,9 @@ const PlatformSwitcher = () => {
     const { t } = useTranslation();
     return (
         <div className='flex cursor-pointer flex-row items-center gap-2 px-base hover:bg-disabled-100'>
-            <img src='/images/pages/header/logo_smart_trader.svg' />
+            <img src='/images/pages/header/logo_smart_trader.svg' alt='Smarttrader logo' />
             <div className='text-base font-bold'>{t('SmartTrader')}</div>
-            <img src='/images/pages/header/ic-chevron-down.svg' />
+            <img src='/images/pages/header/ic-chevron-down.svg' alt='down arrow' />
         </div>
     );
 };
