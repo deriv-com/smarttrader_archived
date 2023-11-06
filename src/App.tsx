81f15d10a31624ec76b40a6b3d0b47dba050dc94
Nada <nada.ajinas@deriv.com>
2023-11-06 15:56:05 +0400

    fix: removed hook call

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import useLogin from 'Api/hooks/useLogin';
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
 
-const App = () => {
-    useLogin();
-    return (
-        <ErrorBoundary>
-            <Layout>App</Layout>
-        </ErrorBoundary>
-    );
-};
+const App = () => (
+    <ErrorBoundary>
+        <Layout>App</Layout>
+    </ErrorBoundary>
+);
 
 export default App;
