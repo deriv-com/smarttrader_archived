8e7cf27e3c3eae11ee999ec4ef263bf296dbe38d
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-11-08 08:53:31 +0400

    fix: test cases fix

--- src/components/layout/header/__test__/PlatformSwitcher.test.tsx
+++ src/components/layout/header/__test__/PlatformSwitcher.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import PlatformSwitcher from '../PlatformSwitcher';
+import userEvent from '@testing-library/user-event';
 
 describe('PlatformSwitcher component', () => {
     test('renders the SmartTrader label', () => {
@@ -9,25 +10,19 @@ describe('PlatformSwitcher component', () => {
         expect(smartTraderLabel).toBeInTheDocument();
     });
 
-    test('renders the "Looking for CFDs? Go to Trader\'s Hub" link', () => {
-        render(<PlatformSwitcher />);
-        const link = screen.getByText("Looking for CFDs? Go to Trader's Hub");
-        expect(link).toBeInTheDocument();
-    });
-
-    test('opens the dropdown menu when clicked', () => {
+    test('opens the dropdown menu when clicked', async () => {
         render(<PlatformSwitcher />);
         const trigger = screen.getByText('SmartTrader');
 
         // Dropdown should be closed initially
-        let dropdownContent = screen.queryByTestId('dropdown-content');
+        let dropdownContent = screen.queryByTestId('dt_dropdown_content');
         expect(dropdownContent).not.toBeInTheDocument();
 
         // Click to open the dropdown
-        trigger.click();
+        await userEvent.click(trigger);
 
         // Dropdown should be open
-        dropdownContent = screen.queryByTestId('dropdown-content');
+        dropdownContent = screen.queryByTestId('dt_dropdown_content');
         expect(dropdownContent).toBeInTheDocument();
     });
 });
