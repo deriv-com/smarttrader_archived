11a5052f8e56a03e6cc40fa0197a4ff38fc3906a
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2023-11-15 12:21:53 +0800

    fix: alias added for jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,16 @@ export default {
     clearMocks: true,
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^Api/(.*)$': '<rootDir>/src/api/$1',
+        '^Components/(.*)$': '<rootDir>/src/components/$1',
+        '^Constants/(.*)$': '<rootDir>/src/constants/$1',
+        '^Contexts/(.*)$': '<rootDir>/src/contexts/$1',
+        '^Styles/(.*)$': '<rootDir>/src/styles/$1',
+        '^Translations$': '<rootDir>/src/translations/$1',
+        '^Types/(.*)$': '<rootDir>/src/types/$1',
+        '^Utils/(.*)$': '<rootDir>/src/utils/$1',
+    },
     transform: {
         '^.+\\.[t|j]sx?$': 'babel-jest',
     },
