2efd98532987a0fca9f9cea1e3e0e032c9db0bc0
Henry Hein <henry@regentmarkets.com>
2023-11-20 10:04:46 +0800

    fix: resolve comments

--- src/api/hooks/useActiveSymbols.ts
+++ src/api/hooks/useActiveSymbols.ts
@@ -3,19 +3,19 @@ import useQuery from './useQuery';
 import useLogin from './useLogin';
 
 const useActiveSymbols = () => {
-    const { is_logged_in, isAuthorized } = useLogin();
-    const { data: active_symbols, ...rest } = useQuery('active_symbols', {
+    const { isLoggedIn, isAuthorized } = useLogin();
+    const { data: activeSymbols, ...rest } = useQuery('active_symbols', {
         payload: { active_symbols: 'brief' },
-        options: { enabled: is_logged_in ? isAuthorized : true },
+        options: { enabled: isLoggedIn ? isAuthorized : true },
     });
 
-    const modified_active_symbols = useMemo(
-        () => ({ ...active_symbols?.active_symbols }),
-        [active_symbols?.active_symbols]
+    const modifiedActiveSymbols = useMemo(
+        () => ({ ...activeSymbols?.active_symbols }),
+        [activeSymbols?.active_symbols]
     );
 
     return {
-        data: modified_active_symbols,
+        data: modifiedActiveSymbols,
         ...rest,
     };
 };
