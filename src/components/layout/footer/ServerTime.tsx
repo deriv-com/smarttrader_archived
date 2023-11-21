b377ac9554ba38f6b4ad84e168f6b6be7789dd9d
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-11-21 14:35:58 +0800

    fix: review comments

--- src/components/layout/footer/ServerTime.tsx
+++ src/components/layout/footer/ServerTime.tsx
@@ -4,12 +4,12 @@ import { epochToLocal, epochToUTC } from 'Utils/moment';
 
 const ServerTime = () => {
     const time = useServerTime();
-    const GMTFormat = epochToUTC(time, 'YYYY-MM-DD HH:mm:ss [GMT]');
+    const UTCFormat = epochToUTC(time, 'YYYY-MM-DD HH:mm:ss [GMT]');
     const localFormat = epochToLocal(time, 'YYYY-MM-DD HH:mm:ss Z');
 
     return (
         <Tooltip className='border-x border-disabled-100 px-2 text-xs' content={localFormat}>
-            <span>{GMTFormat}</span>
+            <span>{UTCFormat}</span>
         </Tooltip>
     );
 };
