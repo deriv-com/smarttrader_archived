94e037b0053cdd8c166ffaad0b075dbb7520e900
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-11-21 13:35:04 +0800

    refactor: improve codes

--- src/components/layout/footer/NetworkStatus.tsx
+++ src/components/layout/footer/NetworkStatus.tsx
@@ -1,18 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import Tooltip from 'Components/common/tooltip';
 import useNetworkStatus from 'Hooks/useNetworkStatus';
+import { useMemo } from 'react';
+
+const statusConfigs = {
+    online: { className: 'bg-success', tooltip: 'Online' },
+    offline: { className: 'bg-danger', tooltip: 'Offline' },
+    blinking: { className: 'animate-pulse bg-success', tooltip: 'Connecting to server' },
+};
 
 const NetworkStatus = () => {
     const status = useNetworkStatus();
     const { t } = useTranslation();
-
-    const statusConfigs = {
-        online: { className: 'bg-success', tooltip: 'Online' },
-        offline: { className: 'bg-danger', tooltip: 'Offline' },
-        blinking: { className: 'animate-pulse bg-success', tooltip: 'Connecting to server' },
-    }[status];
-
-    const { className, tooltip } = statusConfigs;
+    const { className, tooltip } = useMemo(() => statusConfigs[status], [status]);
 
     return (
         <Tooltip className='px-3' content={t('Network status: {{tooltip}}', { tooltip })}>
